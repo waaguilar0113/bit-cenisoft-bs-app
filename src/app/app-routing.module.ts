2c1ca3e0871bd10d570bb3b7867f894a5462d49f
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2021-02-18 22:25:45 -0500

    Added components and routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookComponent } from './pages/book/book.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent
   },
   {
-    path: 'book',
+    path: 'books/:id',
     component: BookComponent
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +9,10 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { BannerComponent } from './layouts/banner/banner.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BookComponent } from './pages/book/book.component';
+import { MenuComponent } from './menu/menu.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,18 @@ import { BookComponent } from './pages/book/book.component';
     HeaderComponent,
     BannerComponent,
     HomeComponent,
-    BookComponent
+    BookComponent,
+    MenuComponent,
+    BookCardComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CommonModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
